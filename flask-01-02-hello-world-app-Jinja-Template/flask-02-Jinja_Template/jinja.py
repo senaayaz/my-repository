d0c4d41f12e7ce85d5c429295bb11535633fec9f
Sena Ayaz <senasayaz@gmail.com>
2022-12-05 22:01:09 -0500

    first flask files

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return render_template('index.html', number1=10, number2=20)
+
+# @app.route('/number/<string:num1>')
+# def number(num1):
+#     return render_template('index.html', number1=num1, number2=20)
+
+@app.route('/sum')
+def number():
+
+    x = 15
+    y = 20
+    return render_template('body.html', value1=x, value2=y, sum=x+y)
+
+if __name__ == '__main__':
+    app.run(debug=True)
